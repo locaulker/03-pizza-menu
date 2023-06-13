b688c158abc690308a18b6249e95a06740d5d811
Lawrence Caulker <locaulker@gmail.com>
2023-06-13 01:57:04 -0400

    implemented the && operator

--- src/index.js
+++ src/index.js
@@ -69,15 +69,21 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData
+  // const pizzas = []
+  const numPizzas = pizzas.length
+
   return (
     <main className='menu'>
       <h2>Our menu</h2>
 
-      <ul className='pizzas'>
-        {pizzaData.map(pizza => (
-          <Pizza pizzaObject={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className='pizzas'>
+          {pizzas.map(pizza => (
+            <Pizza pizzaObject={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
 
       {/* <Pizza
         name='Pizza Spinacci'
@@ -113,14 +119,22 @@ function Pizza(props) {
 
 function Footer() {
   const hour = new Date().getHours()
-  const openHour = 8
-  const closeHour = 22
+  const openHour = -9
+  const closeHour = 5
   const isOpen = hour >= openHour && hour <= closeHour
   console.log(isOpen)
 
   return (
     <footer className='footer'>
-      {new Date().toLocaleTimeString()}. We're currently open!
+      {isOpen && (
+        <div className='order'>
+          <p>
+            We're open until {closeHour} pm. Please come visit us or order
+            online.
+          </p>
+          <button className='btn'>Order</button>
+        </div>
+      )}
     </footer>
   )
   // return React.createElement("footer", null, "We're currently open!")
